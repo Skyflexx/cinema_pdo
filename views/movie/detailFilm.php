00abd09caa4452aab532c5d44564377adbd71f5f
Skyflexx <118121459+Skyflexx@users.noreply.github.com>
2023-06-06 14:18:41 +0200

    #

--- views/movie/detailFilm.php
+++ views/movie/detailFilm.php
@@ -9,12 +9,11 @@
 while ($detail = $detailFilm->fetch()){
 
     echo "<div class ='bg-image' style = 'background-image : url(".$detail['wallpaper']."); width: 100%; height: 100vh;'>
-
-            <div class ='card'><h2>".$detail['titre_film']."</h2>",
-             
-             "<div class='card-body'>".$detail['synopsis'].
-             
-        "</div></div></div>";
+            <div class='container p-5'> <div class ='card'><h2>".$detail['titre_film']."</h2>",             
+                "<div class='card-body'>".$detail['synopsis']."</div>       
+                <div class ='card-body'>Date de sortie : ".$detail['annee_sortie']."</div>
+                <div class ='card-body'>Realisateur : ".$detail['prenom']." ".$detail['nom']." </div>                          
+             </div>";
 
     
 }
